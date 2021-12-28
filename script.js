a4eddbf684289c9d84b83c32bf9ac021b57394ce
ambiscamp <91119798+ambiscamp@users.noreply.github.com>
2021-12-28 09:23:55 +0700

    alert n function update

--- script.js
+++ script.js
@@ -19,3 +19,30 @@ window.addEventListener('scroll', () => {
         navTransparent.classList.add('fixed-top')
     }
 });
+
+// pop up alert jika atribut link href masih # / belum jelas tujuannya
+function notAvailable() {
+    const link = document.querySelectorAll('a')
+    const alertCard = document.querySelector('.my-alert')
+    const elementAlert = document.querySelector(".my-alert span.element-alert")
+    const btnClose = document.querySelector(".my-alert .btn-close")
+    link.forEach(e => {
+        if (e.getAttribute('href') === null && e.getAttribute('name') != null) {
+            elementAlert.innerHTML = "Halaman untuk " + e.getAttribute('name') + " masih dalam tahap pengembangan"
+            alertCard.classList.remove('d-none')
+            setTimeout(() => {
+                alertCard.style.margin = '70px auto 0';
+                alertCard.style.transition = '.5s';
+            }, 0);
+        } else {
+            return
+        }
+    });
+    btnClose.addEventListener('click', () => {
+        alertCard.style.margin = '-180px auto 0';
+        alertCard.style.transition = '.5s';
+        setTimeout(() => {
+            alertCard.classList.add('d-none')
+        }, 500);
+    })
+}
